b53219d4be116ed9c27e0bc28313c754fa7bd70b
asif221b <asif221b@localhost.localdomain>
2020-12-05 15:30:04 -0500

    setting up the project

---
+++ Asif/cmake-build-debug/CMakeFiles/Asif.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Asif"
+  "Asif.pdb"
+  "CMakeFiles/Asif.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Asif.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Asif/cmake-build-debug/CMakeFiles/Asif.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Asif.
+# This may be replaced when dependencies are built.
